a7fa62a00f99aeb418a69c6747e04ef300c023f3
allendehoff <allen.dehoff@colorado.edu>
2016-09-27 12:57:50 -0600

    added files

---
+++ MyFirstBioinformaticsScript.sh
@@ -0,0 +1,11 @@
+#This is my first command
+echo "Hello world."
+
+#This is my second command
+echo "This is something else to do."
+
+#These are my sequence numbers
+seq 1 10 
+
+#This will do something alphabeticaly maybe
+echo (a..z)
